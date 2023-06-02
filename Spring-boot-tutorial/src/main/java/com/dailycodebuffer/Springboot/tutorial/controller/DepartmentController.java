a1eda2d0ad33c1578a1476780792486c638ceb26
220779 <102145019+220779@users.noreply.github.com>
2023-06-02 12:38:17 +0300

    [add] creating Entities

---
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.Springboot.tutorial.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DepartmentController {
+
+
+}
