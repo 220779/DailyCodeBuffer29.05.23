b51e651ba6034d2b824b0bc4d3b548e132a062e8
220779 <102145019+220779@users.noreply.github.com>
2023-06-03 18:16:42 +0300

    [add] Get(APIs)Mapping

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/DepartmentController.java
@@ -2,11 +2,15 @@ package com.dailycodebuffer.Springboot.tutorial.controller;
 
 
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.repository.DepartmentRepository;
 import com.dailycodebuffer.Springboot.tutorial.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -19,4 +23,9 @@ public class DepartmentController {
         return departmentService.saveDepartment(department);
     }
 
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList (){
+        return  departmentService.fetchDepartmentList();
+    }
 }
