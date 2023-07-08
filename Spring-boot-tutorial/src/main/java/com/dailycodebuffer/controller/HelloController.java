36cd2156d8ff9b6e5c8c2379fe14150628b58cc9
220779 <102145019+220779@users.noreply.github.com>
2023-07-08 15:10:50 +0300

    [add] SpringBoot Profiles

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/controller/HelloController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
+    @Autowired
     @Value("${welcome.message}")
     private String welcomeMessage;
-
 
 @GetMapping("/")
     public String helloWorld() {
--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/entity/Department.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/entity/Department.java
@@ -38,6 +38,4 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
-
-
 }
