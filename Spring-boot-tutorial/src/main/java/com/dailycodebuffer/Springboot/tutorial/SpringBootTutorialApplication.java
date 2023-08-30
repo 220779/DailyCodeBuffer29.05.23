cff833c65248c56c3c7f522d57db96525e66ec77
220779 <102145019+220779@users.noreply.github.com>
2023-08-30 10:55:14 +0300

    [correction] deleting .m2 folder, fix JpaRespiratory

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/SpringBootTutorialApplication.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/SpringBootTutorialApplication.java
@@ -1,11 +1,10 @@
 package com.dailycodebuffer.Springboot.tutorial;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableJpaRepositories("com.dailycodebuffer.Springboot.tutorial.repository")  ERROR GIVES!!!!
+//@EnableJpaRepositories("com.dailycodebuffer.Springboot.tutorial.repository") // ERROR GIVES!!!!
 public class SpringBootTutorialApplication {
 
 	public static void main(String[] args) {
--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/DepartmentRepository.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/repository/DepartmentRepository.java
@@ -3,18 +3,16 @@ package com.dailycodebuffer.Springboot.tutorial.repository;
 
 import com.dailycodebuffer.Springboot.tutorial.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-@EnableJpaRepositories
-public interface DepartmentRepository extends JpaRepository<Department, Long>{
+public interface DepartmentRepository extends JpaRepository<Department, Long>
+{
 
 
 
  Department findByDepartmentName(String departmentName);
-
 
  Department findByDepartmentNameIgnoreCase (String departmentName);
 
